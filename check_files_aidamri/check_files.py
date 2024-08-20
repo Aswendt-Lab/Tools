fc4b77b077c3ad541b87eb08b156c19db6b63e2c
arefks <arefks@gmail.com>
2024-08-20 18:00:54 +0200

    corrected

--- check_files_aidamri/check_files.py
+++ check_files_aidamri/check_files.py
@@ -5,7 +5,7 @@ import pandas as pd
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='This script processes NIfTI files in a directory. It extracts relevant parts of the file name and creates a DataFrame.')
-    parser.add_argument('-i', '--input', required=True, help='Path to the parent project folder of the dataset, e.g., raw_data', type=str)
+    parser.add_argument('-i', '--input', required=True, help='Path to the parent project folder of the dataset, e.g., proc_data', type=str)
    
     args = parser.parse_args()
 
@@ -17,16 +17,27 @@ if __name__ == "__main__":
     for tt in temp_files:
         filename = os.path.basename(tt)
         list_split = filename.split("_")
-        Modality = list_split[-1].replace(".nii.gz", "")
-        TimePoint = list_split[-2].replace("ses-", "")
-        SubjectID = list_split[-3].replace("sub-", "")
-
-        data.append({
+        # Initialize the dictionary to collect file details
+        file_info = {
             "FileAddress": tt,
-            "Modality": Modality,
-            "TimePoint": TimePoint,
-            "SubjectID": SubjectID
-        })
+            "Modality": None,
+            "TimePoint": None,
+            "SubjectID": None,
+            "MultipleRun": None
+        }
+
+        # Parse the split filename for specific identifiers
+        for element in list_split:
+            if "sub-" in element:
+                file_info['SubjectID'] = element.replace("sub-", "")
+            elif "ses-" in element:
+                file_info['TimePoint'] = element.replace("ses-", "")
+            elif "run-" in element:
+                file_info['RunNumber'] = element.replace("run-", "")
+            elif ".nii.gz" in element:
+                file_info['Modality'] = element.replace(".nii.gz", "")
+
+        data.append(file_info)
         
     # Create DataFrame
     df = pd.DataFrame(data)
